0b0be8597b03906bb1bf45537fb4d48f743bfd9e
Vidh17 <vidhyakar1721@gmail.com>
2025-04-17 15:39:04 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,10 @@
 import sys
 
+if len(sys.argv) < 3:
+    print("Usage: python test1.py <x> <y>")
+    sys.exit(1)
+
 x = int(sys.argv[1])
 y = int(sys.argv[2])
-print(" Multiply = ", (x*y) )
+
+print(" Multiply = ", (x * y))
